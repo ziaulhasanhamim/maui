13d6b905879e37ad7b17da8c81f870324367edf4
Eilon Lipton <Eilon@users.noreply.github.com>
2021-04-09 12:20:11 -0700

    WIP: .NET MAUI iOS/MacCatalyst

--- src/BlazorWebView/src/Microsoft.AspNetCore.Components.WebView.Maui/iOS/BlazorMauiWebViewHandler.iOS.cs
+++ src/BlazorWebView/src/Microsoft.AspNetCore.Components.WebView.Maui/iOS/BlazorMauiWebViewHandler.iOS.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
 using Foundation;
 using Microsoft.Maui;
 using Microsoft.Maui.Controls;
@@ -16,28 +18,11 @@ namespace Microsoft.AspNetCore.Components.WebView.Maui
 
 		protected override WKWebView CreateNativeView()
 		{
-			return new WKWebView(RectangleF.Empty, CreateConfiguration())
+			return new WKWebView(RectangleF.Empty, new WKWebViewConfiguration())
 			{
 				BackgroundColor = UIColor.Clear,
 				AutosizesSubviews = true
 			};
-		}
-
-		// https://developer.apple.com/forums/thread/99674
-		// WKWebView and making sure cookies synchronize is really quirky
-		// The main workaround I've found for ensuring that cookies synchronize 
-		// is to share the Process Pool between all WkWebView instances.
-		// It also has to be shared at the point you call init
-		static WKWebViewConfiguration CreateConfiguration()
-		{
-			var config = new WKWebViewConfiguration();
-
-			if (SharedPool == null)
-				SharedPool = config.ProcessPool;
-			else
-				config.ProcessPool = SharedPool;
-
-			return config;
 		}
 
 		public override Size GetDesiredSize(double widthConstraint, double heightConstraint)
@@ -45,22 +30,6 @@ namespace Microsoft.AspNetCore.Components.WebView.Maui
 			SetDesiredSize(widthConstraint, heightConstraint);
 
 			return base.GetDesiredSize(widthConstraint, heightConstraint);
-		}
-
-		public void LoadHtml(string? html, string? baseUrl)
-		{
-			if (html != null)
-				TypedNativeView?.LoadHtmlString(html, baseUrl == null ? new NSUrl(NSBundle.MainBundle.BundlePath, true) : new NSUrl(baseUrl, true));
-		}
-
-		public void LoadUrl(string? url)
-		{
-			var uri = new Uri(url ?? string.Empty);
-			var safeHostUri = new Uri($"{uri.Scheme}://{uri.Authority}", UriKind.Absolute);
-			var safeRelativeUri = new Uri($"{uri.PathAndQuery}{uri.Fragment}", UriKind.Relative);
-			NSUrlRequest request = new NSUrlRequest(new Uri(safeHostUri, safeRelativeUri));
-
-			TypedNativeView?.LoadRequest(request);
 		}
 
 		void SetDesiredSize(double width, double height)
@@ -77,28 +46,77 @@ namespace Microsoft.AspNetCore.Components.WebView.Maui
 
 
 
+		private IOSWebViewManager? _webviewManager;
 
+		protected override void DisconnectHandler(WKWebView nativeView)
+		{
+			//nativeView.StopLoading();
 
+			//_webViewClient?.Dispose();
+			//_webChromeClient?.Dispose();
+		}
 
+		private bool RequiredStartupPropertiesSet =>
+			//_webview != null &&
+			HostPage != null &&
+			Services != null;
 
+		public string? HostPage { get; private set; }
+		public ObservableCollection<RootComponent>? RootComponents { get; private set; }
+		public new IServiceProvider? Services { get; private set; }
 
+		private void StartWebViewCoreIfPossible()
+		{
+			if (!RequiredStartupPropertiesSet ||
+				false)//_webviewManager != null)
+			{
+				return;
+			}
+			if (TypedNativeView == null)
+			{
+				throw new InvalidOperationException($"Can't start {nameof(BlazorWebView)} without native web view instance.");
+			}
 
+			var resourceAssembly = RootComponents?[0]?.ComponentType?.Assembly;
+			if (resourceAssembly == null)
+			{
+				throw new InvalidOperationException($"Can't start {nameof(BlazorWebView)} without a component type assembly.");
+			}
 
+			// We assume the host page is always in the root of the content directory, because it's
+			// unclear there's any other use case. We can add more options later if so.
+			var contentRootDir = Path.GetDirectoryName(HostPage) ?? string.Empty;
+			var hostPageRelativePath = Path.GetRelativePath(contentRootDir, HostPage!);
+			var fileProvider = new ManifestEmbeddedFileProvider(resourceAssembly, root: contentRootDir);
 
+			_webviewManager = new IOSWebViewManager(this, TypedNativeView, Services!, MauiDispatcher.Instance, fileProvider, hostPageRelativePath);
+			if (RootComponents != null)
+			{
+				foreach (var rootComponent in RootComponents)
+				{
+					// Since the page isn't loaded yet, this will always complete synchronously
+					_ = rootComponent.AddToWebViewManagerAsync(_webviewManager);
+				}
+			}
+			_webviewManager.Navigate("/");
+		}
 
 		public static void MapHostPage(BlazorWebViewHandler handler, IBlazorWebView webView)
 		{
-			throw new NotImplementedException();
+			handler.HostPage = webView.HostPage;
+			handler.StartWebViewCoreIfPossible();
 		}
 
 		public static void MapRootComponents(BlazorWebViewHandler handler, IBlazorWebView webView)
 		{
-			throw new NotImplementedException();
+			handler.RootComponents = webView.RootComponents;
+			handler.StartWebViewCoreIfPossible();
 		}
 
 		public static void MapServices(BlazorWebViewHandler handler, IBlazorWebView webView)
 		{
-			throw new NotImplementedException();
+			handler.Services = webView.Services;
+			handler.StartWebViewCoreIfPossible();
 		}
 	}
 }
---
+++ src/BlazorWebView/src/Microsoft.AspNetCore.Components.WebView.Maui/iOS/IOSWebViewManager.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.Encodings.Web;
+using Foundation;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Hosting;
+using UIKit;
+using WebKit;
+using RectangleF = CoreGraphics.CGRect;
+
+namespace Microsoft.AspNetCore.Components.WebView.Maui
+{
+	public partial class IOSWebViewManager : WebViewManager, IWKScriptMessageHandler
+	{
+		private const string AppOrigin = "https://0.0.0.0/";
+
+		private readonly BlazorWebViewHandler _blazorMauiWebViewHandler;
+		private readonly WKWebView _webview;
+
+		// TODO: Is this an OK handle to use for IWKScriptMessageHandler.Handle?
+		public IntPtr Handle => NSObject.FromObject(this).Handle;
+
+		public IOSWebViewManager(BlazorWebViewHandler blazorMauiWebViewHandler, WKWebView webview, IServiceProvider services, Dispatcher dispatcher, IFileProvider fileProvider, string hostPageRelativePath)
+			: base(services, dispatcher, new Uri(AppOrigin), fileProvider, hostPageRelativePath)
+		{
+			_blazorMauiWebViewHandler = blazorMauiWebViewHandler ?? throw new ArgumentNullException(nameof(blazorMauiWebViewHandler));
+			_webview = webview ?? throw new ArgumentNullException(nameof(webview));
+
+			InitializeWebView();
+		}
+
+		/// <inheritdoc />
+		protected override void NavigateCore(Uri absoluteUri)
+		{
+			using var nsUrl = new NSUrl(absoluteUri.ToString());
+			using var request = new NSUrlRequest(nsUrl);
+			_webview.LoadRequest(request);
+		}
+
+		/// <inheritdoc />
+		protected override void SendMessage(string message)
+		{
+			var messageJSStringLiteral = JavaScriptEncoder.Default.Encode(message);
+			_webview.EvaluateJavaScript(
+				javascript: $"__dispatchMessageCallback(\"{messageJSStringLiteral}\")",
+				completionHandler: (NSObject result, NSError error) => { });
+		}
+
+		private void InitializeWebView()
+		{
+			var config = _webview.Configuration;
+			config.Preferences.SetValueForKey(NSObject.FromObject(true), new NSString("developerExtrasEnabled"));
+
+			var frameworkScriptSource = @"
+		Blazor.start();
+
+        (function () {
+	        window.onpageshow = function(event) {
+		        if (event.persisted) {
+			        window.location.reload();
+		        }
+	        };
+        })();
+                    ";
+
+			config.UserContentController.AddScriptMessageHandler(this, "webwindowinterop");
+			config.UserContentController.AddUserScript(new WKUserScript(
+				new NSString(frameworkScriptSource), WKUserScriptInjectionTime.AtDocumentEnd, true));
+
+			config.SetUrlSchemeHandler(new SchemeHandler(this), urlScheme: "http");
+
+			_webview.NavigationDelegate = new WebViewNavigationDelegate(_blazorMauiWebViewHandler);
+		}
+
+		public void DidReceiveScriptMessage(WKUserContentController userContentController, WKScriptMessage message)
+		{
+			if (message is null)
+			{
+				throw new ArgumentNullException(nameof(message));
+			}
+
+			MessageReceived(new Uri(AppOrigin), ((NSString)message.Body).ToString());
+		}
+
+		private class SchemeHandler : NSObject, IWKUrlSchemeHandler
+		{
+			private readonly IOSWebViewManager _webViewManager;
+
+			public SchemeHandler(IOSWebViewManager webViewManager)
+			{
+				_webViewManager = webViewManager;
+			}
+
+			[Export("webView:startURLSchemeTask:")]
+			public void StartUrlSchemeTask(WKWebView webView, IWKUrlSchemeTask urlSchemeTask)
+			{
+				var responseBytes = GetResponseBytes(urlSchemeTask.Request.Url.AbsoluteString, out var contentType, statusCode: out var statusCode);
+				if (statusCode == 200)
+				{
+					using (var dic = new NSMutableDictionary<NSString, NSString>())
+					{
+						dic.Add((NSString)"Content-Length", (NSString)(responseBytes.Length.ToString(CultureInfo.InvariantCulture)));
+						dic.Add((NSString)"Content-Type", (NSString)contentType);
+						// Disable local caching. This will prevent user scripts from executing correctly.
+						dic.Add((NSString)"Cache-Control", (NSString)"no-cache, max-age=0, must-revalidate, no-store");
+						using var response = new NSHttpUrlResponse(urlSchemeTask.Request.Url, statusCode, "HTTP/1.1", dic);
+						urlSchemeTask.DidReceiveResponse(response);
+					}
+					urlSchemeTask.DidReceiveData(NSData.FromArray(responseBytes));
+					urlSchemeTask.DidFinish();
+				}
+			}
+
+			private byte[] GetResponseBytes(string url, out string contentType, out int statusCode)
+			{
+				if (_webViewManager.TryGetResponseContent(url, allowFallbackOnHostPage: true, out statusCode, out var statusMessage, out var content, out var headers))
+				{
+					statusCode = 200;
+					using var ms = new MemoryStream();
+					var uri = new Uri(url);
+
+					content.CopyTo(ms);
+					content.Dispose();
+
+					var headersDict =
+						new Dictionary<string, string>(
+						headers
+							.Split(Environment.NewLine)
+							.Select(headerString =>
+								new KeyValuePair<string, string>(
+									headerString.Substring(0, headerString.IndexOf(':')),
+									headerString.Substring(headerString.IndexOf(':') + 2))));
+
+					contentType = headersDict["Content-Type"];
+
+					return ms.ToArray();
+				}
+				else
+				{
+					statusCode = 404;
+					contentType = string.Empty;
+					return Array.Empty<byte>();
+				}
+			}
+
+			[Export("webView:stopURLSchemeTask:")]
+			public void StopUrlSchemeTask(WKWebView webView, IWKUrlSchemeTask urlSchemeTask)
+			{
+			}
+		}
+
+
+		internal class WebViewNavigationDelegate : WKNavigationDelegate
+		{
+			private readonly BlazorWebViewHandler _webView;
+
+			private WKNavigation? _currentNavigation;
+			private Uri? _currentUri;
+
+			public WebViewNavigationDelegate(BlazorWebViewHandler webView)
+			{
+				_webView = webView ?? throw new ArgumentNullException(nameof(webView));
+			}
+
+			public override void DidStartProvisionalNavigation(WKWebView webView, WKNavigation navigation)
+			{
+				_currentNavigation = navigation;
+			}
+
+			public override void DecidePolicy(WKWebView webView, WKNavigationAction navigationAction, Action<WKNavigationActionPolicy> decisionHandler)
+			{
+				if (navigationAction.TargetFrame!.MainFrame)
+				{
+					_currentUri = navigationAction.Request.Url;
+				}
+				decisionHandler(WKNavigationActionPolicy.Allow);
+			}
+
+			public override void DidReceiveServerRedirectForProvisionalNavigation(WKWebView webView, WKNavigation navigation)
+			{
+				// We need to intercept the redirects to the app scheme because Safari will block them.
+				// We will handle these redirects through the Navigation Manager.
+				if (_currentUri?.Host == "0.0.0.0")
+				{
+					var uri = _currentUri;
+					_currentUri = null;
+					_currentNavigation = null;
+					var request = new NSUrlRequest(uri);
+					webView.LoadRequest(request);
+				}
+			}
+
+			public override void DidFailNavigation(WKWebView webView, WKNavigation navigation, NSError error)
+			{
+				_currentUri = null;
+				_currentNavigation = null;
+			}
+
+			public override void DidFailProvisionalNavigation(WKWebView webView, WKNavigation navigation, NSError error)
+			{
+				_currentUri = null;
+				_currentNavigation = null;
+			}
+
+			public override void DidCommitNavigation(WKWebView webView, WKNavigation navigation)
+			{
+				if (_currentUri != null && _currentNavigation == navigation)
+				{
+					//_webView.HandleNavigationStarting(_currentUri);
+				}
+			}
+
+			public override void DidFinishNavigation(WKWebView webView, WKNavigation navigation)
+			{
+				if (_currentUri != null && _currentNavigation == navigation)
+				{
+					//_webView.HandleNavigationFinished(_currentUri);
+					_currentUri = null;
+					_currentNavigation = null;
+				}
+			}
+		}
+	}
+}
