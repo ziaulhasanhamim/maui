3bea70f62bdeef0f95c63f158c675c5a42bdb88e
Shane Neuville <shneuvil@microsoft.com>
2021-06-15 15:59:55 -0500

    - project name fixes

--- src/Compatibility/Core/src/Properties/AssemblyInfo.cs
+++ src/Compatibility/Core/src/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Compatibility.Windows.UnitTests")]
 [assembly: InternalsVisibleTo("Compatibility.Android.UnitTests")]
 [assembly: InternalsVisibleTo("Compatibility.UnitTests")]
+[assembly: InternalsVisibleTo("Microsoft.Maui.Controls.Compatibility.UnitTests")]
