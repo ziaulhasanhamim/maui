2399e1782abc51ed2c79c5ba6221892721fedcd7
Eilon Lipton <Eilon@users.noreply.github.com>
2021-07-27 10:55:32 -0700

    Replace FontCollectionBuilder with startup services and builders

--- src/Core/src/Core/IStartup.cs
+++ src/Core/src/Core/IStartup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -47,8 +48,66 @@ namespace Microsoft.Maui
 
 		public MauiAppBuilder ConfigureFonts(Action<IFontCollection> configureDelegate)
 		{
-			ConfigureServices<FontCollectionBuilder>((_, fonts) => configureDelegate(fonts));
+			Services.AddSingleton<IEmbeddedFontLoader>(svc => new EmbeddedFontLoader(svc.CreateLogger<EmbeddedFontLoader>()));
+			Services.AddSingleton<IFontRegistrar>(svc => new FontRegistrar(svc.GetRequiredService<IEmbeddedFontLoader>(), svc.CreateLogger<FontRegistrar>()));
+			Services.AddSingleton<IFontManager>(svc => new FontManager(svc.GetRequiredService<IFontRegistrar>(), svc.CreateLogger<FontManager>()));
+
+			Services.AddSingleton<FontsRegistration>(new FontsRegistration(configureDelegate));
+			Services.AddSingleton<IMauiInitializeService, FontInitializer>();
 			return this;
+		}
+
+
+		internal class FontsRegistration
+		{
+			private readonly Action<FontCollection> _registerFonts;
+
+			public FontsRegistration(Action<FontCollection> registerFonts)
+			{
+				_registerFonts = registerFonts;
+			}
+
+			internal void AddFonts(FontCollection fonts)
+			{
+				_registerFonts(fonts);
+			}
+		}
+
+		internal class FontInitializer : IMauiInitializeService
+		{
+			private readonly IEnumerable<FontsRegistration> _fontsRegistrations;
+
+			public FontInitializer(IEnumerable<FontsRegistration> fontsRegistrations)
+			{
+				_fontsRegistrations = fontsRegistrations;
+			}
+
+			public void Initialize(HostBuilderContext context, IServiceProvider services)
+			{
+				var fontRegistrar = services.GetService<IFontRegistrar>();
+				if (fontRegistrar == null)
+					return;
+
+				if (_fontsRegistrations != null)
+				{
+					var fontsBuilder = new FontCollection();
+
+					// Run all the user-defined registrations
+					foreach (var font in _fontsRegistrations)
+					{
+						font.AddFonts(fontsBuilder);
+					}
+
+					// Register the fonts in the registrar
+					foreach (var font in fontsBuilder)
+					{
+						if (font.Assembly == null)
+							fontRegistrar.Register(font.Filename, font.Alias);
+						else
+							fontRegistrar.Register(font.Filename, font.Alias, font.Assembly);
+					}
+				}
+			}
 		}
 
 		readonly List<Action<HostBuilderContext, IConfigurationBuilder>> _configureAppConfigActions = new List<Action<HostBuilderContext, IConfigurationBuilder>>();
@@ -105,34 +164,6 @@ namespace Microsoft.Maui
 			{
 				services.AddSingleton<IImageSourceServiceConfiguration, ImageSourceServiceConfiguration>();
 				services.AddSingleton<IImageSourceServiceProvider>(svcs => new ImageSourceServiceProvider(this, svcs));
-			}
-		}
-
-
-
-		class FontCollectionBuilder : FontCollection, IMauiServiceBuilder, IMauiInitializeService
-		{
-			public void ConfigureServices(HostBuilderContext context, IServiceCollection services)
-			{
-				services.AddSingleton<IEmbeddedFontLoader>(svc => new EmbeddedFontLoader(svc.CreateLogger<EmbeddedFontLoader>()));
-				services.AddSingleton<IFontRegistrar>(svc => new FontRegistrar(svc.GetRequiredService<IEmbeddedFontLoader>(), svc.CreateLogger<FontRegistrar>()));
-				services.AddSingleton<IFontManager>(svc => new FontManager(svc.GetRequiredService<IFontRegistrar>(), svc.CreateLogger<FontManager>()));
-				services.InitThis(this);
-			}
-
-			public void Initialize(HostBuilderContext context, IServiceProvider services)
-			{
-				var fontRegistrar = services.GetService<IFontRegistrar>();
-				if (fontRegistrar == null)
-					return;
-
-				foreach (var font in this)
-				{
-					if (font.Assembly == null)
-						fontRegistrar.Register(font.Filename, font.Alias);
-					else
-						fontRegistrar.Register(font.Filename, font.Alias, font.Assembly);
-				}
 			}
 		}
 
